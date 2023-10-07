cd27dd3c22971ca0d188ec61981198b273d15903
bmc-eng <bclarkeuk@googlemail.com>
2023-10-07 20:35:52 +0100

    Updated recipe structure to support bson

--- main.go
+++ main.go
@@ -11,20 +11,20 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 type Recipe struct {
-	ID           string    `json:"id"`
-	Name         string    `json:"name"`
-	Tags         []string  `json:"tags"`
-	Ingredients  []string  `json:"ingredients"`
-	Instructions []string  `json:"instructions"`
-	PublishedAt  time.Time `json:"publishedAt"`
+	ID           primitive.ObjectID `json:"id" bson:"_id"`
+	Name         string             `json:"name" bson:"name"`
+	Tags         []string           `json:"tags" bson:"tags"`
+	Ingredients  []string           `json:"ingredients" bson:"ingredients"`
+	Instructions []string           `json:"instructions" bson:"instructions"`
+	PublishedAt  time.Time          `json:"publishedAt" bson:"publishedAt"`
 }
 
 // Global Variables
@@ -93,14 +93,20 @@ func ListRecipesHandler(c *gin.Context) {
 
 func SingleRecipeHandler(c *gin.Context) {
 	id := c.Param("id")
-	cur, err := collection.Find(ctx, bson.M{"_id": id})
+
+	// Convert to primitive object
+	objectId, _ := primitive.ObjectIDFromHex(id)
+	cur, err := collection.Find(ctx, bson.M{"_id": objectId})
 	if err != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
 		return
 	}
 	defer cur.Close(ctx)
 	var recipe Recipe
-	cur.Decode(&recipe)
+	for cur.Next(ctx) {
+		cur.Decode(&recipe)
+	}
+
 	c.JSON(http.StatusOK, recipe)
 }
 
@@ -138,7 +144,7 @@ func NewRecipeHandler(c *gin.Context) {
 			"error": err.Error()})
 		return
 	}
-	recipe.ID = uuid.New().String()
+	recipe.ID = primitive.NewObjectID()
 	recipe.PublishedAt = time.Now()
 	_, err = collection.InsertOne(ctx, recipe)
 	if err != nil {
@@ -167,7 +173,8 @@ func UpdateRecipeHandler(c *gin.Context) {
 	}
 
 	// Find the recipe that matches the index
-	_, err = collection.UpdateOne(ctx, bson.M{"_id": id}, bson.D{{"$set", bson.D{
+	objectId, _ := primitive.ObjectIDFromHex(id)
+	_, err = collection.UpdateOne(ctx, bson.M{"_id": objectId}, bson.D{{"$set", bson.D{
 		{"name", recipe.Name},
 		{"instructions", recipe.Instructions},
 		{"ingredients", recipe.Ingredients},
@@ -183,6 +190,7 @@ func UpdateRecipeHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "Recipe has been updated"})
 }
 
+// this is now broken under id
 func DeleteRecipeHandler(c *gin.Context) {
 	// get the id from the parameter passed into the url
 	id := c.Param("id")
@@ -190,7 +198,8 @@ func DeleteRecipeHandler(c *gin.Context) {
 	// find the id from the list of recipes
 	index := -1
 	for i := 0; i < len(recipes); i++ {
-		if recipes[i].ID == id {
+		objectId, _ := primitive.ObjectIDFromHex(id)
+		if recipes[i].ID == objectId {
 			index = i
 			break
 		}
